e059bd886e443274d0bd71f8796df7aff288d12e
Alex-Uva89 <a.uva1988@gmail.com>
2025-09-05 16:37:50 +0200

    FIX favicon

--- quasar.config.js
+++ quasar.config.js
@@ -171,8 +171,6 @@ export default defineConfig((ctx) => {
           { src: 'icons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
 
           // opzionali ma utili per compatibilità/launcher
-          { src: 'icons/favicon-128x128.png', sizes: '128x128', type: 'image/png' },
-          { src: 'icons/favicon-96x96.png',  sizes: '96x96',  type: 'image/png' },
           { src: 'icons/favicon-32x32.png',  sizes: '32x32',  type: 'image/png' },
           { src: 'icons/favicon-16x16.png',  sizes: '16x16',  type: 'image/png' }
         ]
