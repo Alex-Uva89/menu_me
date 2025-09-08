4a6aca6edb571eed937c4fd0b0dc4da8d3c524ec
Alex-Uva89 <a.uva1988@gmail.com>
2025-09-08 10:24:55 +0200

    FIX CategoryTitle

--- src/components/menu/CategoryTitle.vue
+++ src/components/menu/CategoryTitle.vue
@@ -4,9 +4,6 @@
     <div class="text-subtitle1 text-weight-bold ellipsis">
       {{ resolvedTitle }}
     </div>
-    <div v-if="resolvedSubtitle" class="text-caption text-grey-7 ellipsis">
-      {{ resolvedSubtitle }}
-    </div>
   </div>
 </template>
 
@@ -14,11 +11,11 @@
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useCategoriesStore } from 'stores/categories'
-import { useBusinessStore } from 'stores/business'
+
 
 const { t, locale } = useI18n()
 const categories = useCategoriesStore()
-const business = useBusinessStore()
+
 
 const selected = computed(() => categories.categorySelected || categories.currentParent)
 function localize(cat) {
@@ -27,5 +24,4 @@ function localize(cat) {
 }
 
 const resolvedTitle = computed(() => localize(selected.value) || t('selectCategory'))
-const resolvedSubtitle = computed(() => business.current?.name || '')
 </script>
